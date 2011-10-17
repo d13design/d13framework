9b2f57dac538b8a10d89b16772fb64d328877453
Dave Waller <davewallr@gmail.com>
2011-10-17 16:44:48 +0100

    Update to support querystring data passing to sections

--- lib/route.inc.php
+++ lib/route.inc.php
@@ -1,5 +1,6 @@
 <?php
-// 
+
+// 
 //  route.inc.php
 //  d13design
 //  
@@ -11,8 +12,18 @@
 $u = $_SERVER['REQUEST_URI'];
 $u = str_replace($path,"",$u);
 $t = preg_split('[\\/]', $u, -1, PREG_SPLIT_NO_EMPTY);
-$a['section'] = $t[0];
+$section = explode("?",$t[0]);
+$a['section'] = $section[0];
+$a['qs'] = $section[1];
 $a['page'] = $t[1];
-$a['item'] = $t[2];
+$a['items'] = array();
+for($b=2;$b<count($t);$b++){ $a['items'][] = $t[$b]; }
+
+if(count($a['items'])==0) unset($a['items']);
+if($a['page']=='') unset($a['page']);
+if($a['qs']=='') unset($a['qs']);
+if($a['section']=='') unset($a['section']);
+
+//print_r($a);
 
 ?>
