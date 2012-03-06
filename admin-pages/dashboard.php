2d6b5bf93c2a987ff3c2ab0756b6171dfbf92f70
Dave Waller <davewallr@gmail.com>
2012-03-06 17:34:59 +0000

    Default template tidy up

--- admin-pages/dashboard.php
+++ admin-pages/dashboard.php
@@ -12,3 +12,13 @@
 </ul>
 
 <p>Welcome back <strong><?php echo $_SESSION['username']; ?></strong>.</p>
+
+<div class="page-header">
+	<h3>Terminology</h3>
+</div>
+
+<p><strong>Articles</strong> are your posts - these could be news articles, blog posts or anything else you'd like to include in your site.</p>
+<p><strong>Pages</strong> are static content pages on your site - an about page or a contact page for example.</p>
+<p><strong>Sections</strong> are the folders where your articles are stored - give your site structure using these sections.</p>
+<p><strong>Templates</strong> define how your site looks, use the help guide to create your own.</p>
+<p><strong>Slugs</strong> are simplified titles for your content that can be used to create search engine friendly URLs, typically these are the same as the title but replace spaces with hyphens.</p>
